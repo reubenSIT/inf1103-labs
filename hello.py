6b2632e163fc4ab96ea5f9354300bc15f507a039
Reuben <2601484@sit.singaporetech.edu.sg>
2026-09-04 15:28:10 +0800

    Dynamic Profile

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ print("Bio:", bio)
 print("Followers:", followers)'''
 
 
-followers = 100
+'''followers = 100
 
 followers += 50
 print("Day 1:", followers)
@@ -21,4 +21,15 @@ followers += 20
 print("Day 2:", followers)
 
 followers += 10
-print("Day 3:", followers)
+print("Day 3:", followers)'''
+
+
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
