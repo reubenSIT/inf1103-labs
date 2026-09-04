ee898dee7099fec5a3447eba0a36b93ba0c097b7
Reuben <2601484@sit.singaporetech.edu.sg>
2026-09-04 15:24:02 +0800

    Assignment Variables

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-print("=================")
+''' print("=================")
 print("Welcome here")
 print("My first post")
 print("=================")
@@ -9,4 +9,16 @@ followers = 100
 
 print("Username:", username)
 print("Bio:", bio)
-print("Followers:", followers)
+print("Followers:", followers)'''
+
+
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
