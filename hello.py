6b7e977ec5bee67cd794ec39b6a288ebb6376e86
Reuben <2601484@sit.singaporetech.edu.sg>
2026-09-04 15:33:02 +0800

    Fun conditions

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
+# Act 1
 ''' print("=================")
 print("Welcome here")
 print("My first post")
 print("=================")
 
+# Act 2
 username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
@@ -11,7 +13,7 @@ print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)'''
 
-
+# Act 3
 '''followers = 100
 
 followers += 50
@@ -23,8 +25,8 @@ print("Day 2:", followers)
 followers += 10
 print("Day 3:", followers)'''
 
-
-username = input("Enter Username: ")
+# Act 4
+'''username = input("Enter Username: ")
 age = input("Enter Age: ")
 category = input("Enter Content Category: ")
 
@@ -32,4 +34,20 @@ print("\nInstagram Profile")
 print("====================")
 print("Username:", username)
 print("Age:", age)
+print("Category:", category) '''
+
+
+
+# Act 5
+username = input("Enter Username: ")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
 print("Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
